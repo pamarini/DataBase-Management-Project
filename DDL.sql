ad959e21d5daa87adc23315869f5eed6993d3a00
pamarini <peter.marinis96@gmail.com>
2017-11-24 15:39:52 -0500

    adding tables to DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,35 +1,64 @@
--- CANADIAN TIRE DATA RELATIONS --
-
-								-- dept_name, 
-insert into department values ('Automotive');
-insert into department values ('HouseWare');
-insert into department values ('SportingGood');
-insert into department values ('Outdoor');
-insert into department values ('Hardware'); 
-insert into department values ('Paint' );
-
-							-- dept_name, category, type
-insert into product values ('Automotive', 'Tires', 'Winter' );
-insert into product values ('Automotive', 'Tires', 'Summer' );
-insert into product values ('Automotive', 'Tires', 'AllSeason' );
-
-insert into product values ('Automotive', 'Lights', 'Signal' );
-insert into product values ('Automotive', 'Lights', 'Break' );
-insert into product values ('Automotive', 'Lights', 'Head' );
-
-insert into product values ('HouseWare', 'Clean', 'Broom' );
-insert into product values ('HouseWare', 'Clean', 'Mop' );
-insert into product values ('HouseWare', 'Clean', 'Vacuum' );
-insert into product values ('HouseWare', 'Clean', 'Sponge' );
-
-insert into product values ('SportingGood', 'Hunting' );
-insert into product values ('SportingGood', 'Fishing' );
-insert into product values ('SportingGood', 'Hockey' );
-insert into product values ('SportingGood', 'Boating' );
+create schema cs3413_final;   	# change userID with your own user id
+use cs3413_final;				# change userID with your own user id
 
 
+create table brand
+	(id				varchar(8),
+	 brand_name		varchar(15),
+	 primary key (id)
+	) ENGINE=INNODB;
 
+create table product
+	(upc			varchar(20), 
+	 product_name	varchar(15), 
+     size			varchar(15),
+     primary key (upc)
+	)ENGINE=INNODB;
 
+create table category
+	(id				varchar(8), 
+	 category_name	varchar(20),
+	 primary key (id)
+	) ENGINE=INNODB;
+
+create table vendor
+	(id				varchar(8), 
+	 vendor_name	varchar(20),
+	 primary key (id)
+	);
+
+create table shipment
+	(order_no		numeric(0,9), 
+	 upc			varchar(8),
+	 quantity		numeric(0,9),
+     unit_price		numeric(0,9),
+	 primary key (order_no)
+	);
+
+create table store
+	(id				varchar(8), 
+	 store_name		varchar(25),
+	 city			varchar(20), 
+	 address		varchar(20),
+	 year			numeric(4,0),
+	 primary key (id)
+	);
+
+create table ordering
+	(id			varchar(8), 
+	 primary key (id)
+	);
+
+create table customer
+	(id				varchar(8), 
+	 address		varchar(25), 
+	 country		varchar(25),
+	 state_prov		varchar(25),
+     postal_zip		numeric(0,9),
+	 st_address		varchar(20),
+	 primary key (ID)
+
+	);
 
 
 
