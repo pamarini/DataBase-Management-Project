534246b28c7d2a1585e4444937dfff0184df2d74
pamarini <peter.marinis96@gmail.com>
2017-11-29 16:55:34 -0500

    added stocks input

--- insert.sql
+++ insert.sql
@@ -60,7 +60,7 @@ insert into products values (88117, 'All Season Tires', 17, 179.99, 1 );
 insert into products values (88118, 'All Season Tires', 17, 199.99, 2 );		
 insert into products values (88119, 'All Season Tires', 18, 209.99, 1 );		
 insert into products values (88120, 'All Season Tires', 18, 229.99, 2 );
-insert into products values (88120, 'All Season Tires', 18, 259.99, 5 );
+insert into products values (88121, 'All Season Tires', 18, 259.99, 5 );
 
 		-- TOOLS&HARDWARE	 -- upc,  product_name, size,  msrp, brand_id
 insert into products values (88201, 'Screwdriver', 'Slot', 4.99, 6 );		-- brand_id 6-9 = all tool brands
@@ -88,16 +88,78 @@ insert into products values (88402, 'Hockey Skates', 9, 54.99, 18 );		-- brand_i
 insert into products values (88403, 'Hockey Skates', 10, 69.99, 17 );		-- brand_id 21 = schwiin
 insert into products values (88404, 'Hockey Skates', 10, 69.99, 18 );
 insert into products values (88410, 'Bikes', 28, 129.99, 18 );
-insert into products values (88411, 'Bikes', 28, 149,99, 21 );
-insert into products values (88412, 'Bikes', 29, 169,99, 18 );
-insert into products values (88413, 'Bikes', 29, 209,99, 21 );
-insert into products values (88414, 'Bikes', 31, 189,99, 18 );
-insert into products values (88415, 'Bikes', 31, 249,99, 21 );
+insert into products values (88411, 'Bikes', 28, 149.99, 21 );
+insert into products values (88412, 'Bikes', 29, 169.99, 18 );
+insert into products values (88413, 'Bikes', 29, 209.99, 21 );	
+insert into products values (88414, 'Bikes', 31, 189.99, 18 );
+insert into products values (88415, 'Bikes', 31, 249.99, 21 );
 
 	-- SEASONAL				--upc,	product_name   ,size, msrp,	brand_id
 insert into products values (88501, 'Lawn Mower', 'Small', 299.99, 21 );
 insert into products values (88502, 'Lawn Mower', 'Medium', 399.99, 21 );		-- brand_id 21 = yardworks
 insert into products values (88503, 'Lawn Mower', 'Large', 429.99, 21  );
+
+
+	-- AUTOMOTIVE STOCK
+insert into stock values (88101, vendor_id, store_id, quantity, 179.99);
+insert into stock values (88102, vendor_id, store_id, quantity, 199.99);
+insert into stock values (88103, vendor_id, store_id, quantity, 219.99);
+insert into stock values (88104, vendor_id, store_id, quantity, 239.99);
+insert into stock values (88105, vendor_id, store_id, quantity, 259.99);
+insert into stock values (88106, vendor_id, store_id, quantity, 299.99);
+insert into stock values (88107, vendor_id, store_id, quantity, 349.99);
+insert into stock values (88108, vendor_id, store_id, quantity, 129.99);
+insert into stock values (88109, vendor_id, store_id, quantity, 149.99);
+insert into stock values (88110, vendor_id, store_id, quantity, 169.99);
+insert into stock values (88111, vendor_id, store_id, quantity, 189.99);
+insert into stock values (88112, vendor_id, store_id, quantity, 199.99);
+insert into stock values (88113, vendor_id, store_id, quantity, 219.99);
+insert into stock values (88114, vendor_id, store_id, quantity, 249.99);
+insert into stock values (88115, vendor_id, store_id, quantity, 139.99);
+insert into stock values (88116, vendor_id, store_id, quantity, 159.99);
+insert into stock values (88117, vendor_id, store_id, quantity, 179.99);
+insert into stock values (88118, vendor_id, store_id, quantity, 199.99);
+insert into stock values (88119, vendor_id, store_id, quantity, 209.99);
+insert into stock values (88120, vendor_id, store_id, quantity, 229.99);
+insert into stock values (88121, vendor_id, store_id, quantity, 259.99);
+
+	-- TOOLS&HARDWARE STOCK 
+insert into stock values (88201, vendor_id, store_id, quantity, 4.99);
+insert into stock values (88202, vendor_id, store_id, quantity, 5.99);
+insert into stock values (88203, vendor_id, store_id, quantity, 6.99);
+insert into stock values (88204, vendor_id, store_id, quantity, 7.99);
+insert into stock values (88205, vendor_id, store_id, quantity, 10.99);
+insert into stock values (88206, vendor_id, store_id, quantity, 11.99);
+insert into stock values (88207, vendor_id, store_id, quantity, 12.99);
+insert into stock values (88208, vendor_id, store_id, quantity, 13.99);
+insert into stock values (88209, vendor_id, store_id, quantity, 10.49);
+insert into stock values (88210, vendor_id, store_id, quantity, 11.49);
+insert into stock values (88211, vendor_id, store_id, quantity, 12.49);
+insert into stock values (88212, vendor_id, store_id, quantity, 13.49);
+
+	-- HOME&ESSENTIAL STOCK
+insert into stock values (88301, vendor_id, store_id, quantity, 6.99);
+insert into stock values (88302, vendor_id, store_id, quantity, 15.99);
+insert into stock values (88303, vendor_id, store_id, quantity, 7.99);
+insert into stock values (88304, vendor_id, store_id, quantity, 16.99);
+
+	-- SPORTS&RECREATIONAL STOCK
+insert into stock values (88401, vendor_id, store_id, quantity, 69.99);
+insert into stock values (88402, vendor_id, store_id, quantity, 59.99);
+insert into stock values (88403, vendor_id, store_id, quantity, 69.99);
+insert into stock values (88404, vendor_id, store_id, quantity, 69.99);
+insert into stock values (88410, vendor_id, store_id, quantity, 129.99);
+insert into stock values (88411, vendor_id, store_id, quantity, 149.99);
+insert into stock values (88412, vendor_id, store_id, quantity, 169.99);
+insert into stock values (88413, vendor_id, store_id, quantity, 209.99);
+insert into stock values (88414, vendor_id, store_id, quantity, 189.99);
+insert into stock values (88414, vendor_id, store_id, quantity, 249.99);
+
+	-- SEASONAL STOCK
+insert into stock values (88501, vendor_id, store_id, quantity, 299.99);
+insert into stock values (88502, vendor_id, store_id, quantity, 399.99);
+insert into stock values (88503, vendor_id, store_id, quantity, 429.99);
+
 
 						-- id,		name,		address_id
 insert into stores values (id, 'TBay North', address_id);
