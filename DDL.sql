d288c7602f88772a882af752e219366595bbbed1
Daniel Kivi <sugarfreedanny@gmail.com>
2017-11-29 01:47:49 -0500

    Changed a lot of the DDL file  - Added relationship tables  - Added all the foreign keys  - Made addresses it's own table  - Made everything not null  - Made all strings 50 characters  - Made prices 15 digits with 2 decimals
    Added Database Schema Image

--- DDL.sql
+++ DDL.sql
@@ -1,64 +1,169 @@
-create schema cs3413_final;   	# change userID with your own user id
-use cs3413_final;				# change userID with your own user id
+CREATE DATABASE IF NOT EXISTS cs3413_final;
+
+USE cs3413_final;
+
+CREATE TABLE addresses
+(
+  id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  address    VARCHAR(50) NOT NULL,
+  address2   VARCHAR(50) NOT NULL,
+  city       VARCHAR(50) NOT NULL,
+  state_prov VARCHAR(50) NOT NULL,
+  country    VARCHAR(50) NOT NULL,
+  postal_zip VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE brands
+(
+  id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products
+(
+  upc          VARCHAR(12)    NOT NULL PRIMARY KEY,
+  product_name VARCHAR(50)    NOT NULL,
+  size         VARCHAR(50)    NOT NULL,
+  msrp         DECIMAL(15, 2) NOT NULL,
+  brand_id     INT            NOT NULL,
+  FOREIGN KEY (brand_id) REFERENCES brands (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE categories
+(
+  id            INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50)
+);
+
+CREATE TABLE category_product
+(
+  category_id INT         NOT NULL,
+  product_upc VARCHAR(12) NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (product_upc) REFERENCES products (upc)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  PRIMARY KEY (category_id, product_upc)
+);
+
+CREATE TABLE vendors
+(
+  id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE brand_vendor
+(
+  brand_id  INT NOT NULL,
+  vendor_id INT NOT NULL,
+  FOREIGN KEY (brand_id) REFERENCES brands (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (vendor_id) REFERENCES vendors (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  PRIMARY KEY (brand_id, vendor_id)
+);
+
+CREATE TABLE stores
+(
+  id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name       VARCHAR(50) NOT NULL,
+  address_id INT         NOT NULL,
+  FOREIGN KEY (address_id) REFERENCES addresses (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE stock
+(
+  product_upc VARCHAR(12)    NOT NULL,
+  vendor_id   INT            NOT NULL,
+  store_id    INT            NOT NULL,
+  quantity    INT            NOT NULL,
+  price       DECIMAL(15, 2) NOT NULL,
+  FOREIGN KEY (product_upc) REFERENCES products (upc)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (vendor_id) REFERENCES vendors (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (store_id) REFERENCES stores (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  PRIMARY KEY (product_upc, vendor_id, store_id)
+);
+
+CREATE TABLE shipments
+(
+  id           INT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  vendor_id    INT      NOT NULL,
+  store_id     INT      NOT NULL,
+  sent_date    DATETIME NOT NULL,
+  recieve_date DATETIME NOT NULL,
+  FOREIGN KEY (vendor_id) REFERENCES vendors (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (store_id) REFERENCES stores (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE product_shipment
+(
+  shipment_id INT            NOT NULL,
+  product_upc VARCHAR(12)    NOT NULL,
+  quantity    INT            NOT NULL,
+  unit_price  DECIMAL(15, 2) NOT NULL,
+  FOREIGN KEY (shipment_id) REFERENCES vendors (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (product_upc) REFERENCES products (upc)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  PRIMARY KEY (shipment_id, product_upc)
+);
+
+CREATE TABLE customers
+(
+  id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email      VARCHAR(50) NOT NULL,
+  password   VARCHAR(50) NOT NULL,
+  address_id INT         NOT NULL,
+  FOREIGN KEY (address_id) REFERENCES addresses (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE orders
+(
+  id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE order_product
+(
+  order_id    INT            NOT NULL,
+  product_upc VARCHAR(12)    NOT NULL,
+  quantity    INT            NOT NULL,
+  unit_price  DECIMAL(15, 2) NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (product_upc) REFERENCES products (upc)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  PRIMARY KEY (order_id, product_upc)
+)
 
 
-create table brand
-	(id				varchar(8),
-	 brand_name		varchar(15),
-	 primary key (id)
-	) ENGINE=INNODB;
-
-create table product
-	(upc			varchar(20), 
-	 product_name	varchar(15), 
-     size			varchar(15),
-     primary key (upc)
-	)ENGINE=INNODB;
-
-create table category
-	(id				varchar(8), 
-	 category_name	varchar(20),
-	 primary key (id)
-	) ENGINE=INNODB;
-
-create table vendor
-	(id				varchar(8), 
-	 vendor_name	varchar(20),
-	 primary key (id)
-	);
-
-create table shipment
-	(order_no		numeric(0,9), 
-	 upc			varchar(8),
-	 quantity		numeric(0,9),
-     unit_price		numeric(0,9),
-	 primary key (order_no)
-	);
-
-create table store
-	(id				varchar(8), 
-	 store_name		varchar(25),
-	 city			varchar(20), 
-	 address		varchar(20),
-	 year			numeric(4,0),
-	 primary key (id)
-	);
-
-create table ordering
-	(id			varchar(8), 
-	 primary key (id)
-	);
-
-create table customer
-	(id				varchar(8), 
-	 address		varchar(25), 
-	 country		varchar(25),
-	 state_prov		varchar(25),
-     postal_zip		numeric(0,9),
-	 st_address		varchar(20),
-	 primary key (ID)
-
-	);
 
 
 
