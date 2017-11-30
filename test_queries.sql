d7fe7e660bd1a5df7d029b03ebd14da75b6c350e
Daniel Kivi <sugarfreedanny@gmail.com>
2017-11-30 09:55:49 -0500

    Test Query Two

--- test_queries.sql
+++ test_queries.sql
@@ -14,3 +14,19 @@ FROM (
   INNER JOIN stores ON A.store_id = stores.id
 GROUP BY product_upc
 ORDER BY store_id, sum(quantity) DESC;
+
+SELECT
+  addresses.city,
+  product_upc,
+  products.product_name,
+  sum(quantity) AS amount_sold
+FROM (
+       SELECT *
+       FROM orders
+         LEFT JOIN order_product ON orders.id = order_product.order_id
+     ) AS A
+  INNER JOIN products ON A.product_upc = products.upc
+  INNER JOIN stores ON A.store_id = stores.id
+  INNER JOIN addresses ON stores.address_id = addresses.id
+GROUP BY product_upc
+ORDER BY addresses.city, sum(quantity) DESC;
